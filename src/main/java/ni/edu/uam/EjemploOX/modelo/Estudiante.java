663db13e09dc25f2bf4fa8734a153a22c062783a
vybdani <darochap@uamv.edu.ni>
2026-05-22 17:39:54 -0600

    Clase Estudiante

--- src/main/java/ni/edu/uam/EjemploOX/modelo/Estudiante.java
+++ src/main/java/ni/edu/uam/EjemploOX/modelo/Estudiante.java
@@ -1,1 +1,25 @@
+package ni.edu.uam.EjemploOX.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private LocalDate fechaNac;
+}
